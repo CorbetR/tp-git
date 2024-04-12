d0e9b7b0c19099e42884f3f1c11d2227bbb7d1d4
Rayan Corbet <rayancorbet@outlook.fr>
2024-04-12 16:16:19 +0200

    Test pyest

---
+++ test_calculator.py
@@ -0,0 +1,12 @@
+from calculator import addition
+
+
+
+def test_addition():
+    assert addition(1, 2) == 3
+
+def test_soustraction():
+    assert addition(5, 2) == 3
+
+def test_division():
+    assert addition(4, 2) == 2
