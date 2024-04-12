9482d20d044c5b07dfce671d2098fba81946ed8f
Rayan Corbet <rayancorbet@outlook.fr>
2024-04-12 16:05:01 +0200

    Test pylint

--- calculator.py
+++ calculator.py
@@ -1,11 +1,41 @@
 def addition(a, b):
-    result = a+b
+    """
+    Return the sum of two numbers.
+
+    Parameters:
+    a (int or float): The first number.
+    b (int or float): The second number.
+
+    Returns:
+    int or float: The result of the addition.
+    """
+    result = a + b
     return result
 
 def soustraction(a, b):
-    result = a-b
+    """
+    Return the difference between two numbers.
+
+    Parameters:
+    a (int or float): The first number.
+    b (int or float): The second number.
+
+    Returns:
+    int or float: The result of the subtraction.
+    """
+    result = a - b
     return result
 
 def division(a, b):
-    result = a/b
+    """
+    Return the result of dividing two numbers.
+
+    Parameters:
+    a (int or float): The dividend.
+    b (int or float): The divisor.
+
+    Returns:
+    float: The result of the division.
+    """
+    result = a / b
     return result
